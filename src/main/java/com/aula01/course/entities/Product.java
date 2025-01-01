1380469e70e92a111576aa18fbbadab39562c2bb
jaderAdriel <jaderadriel7@gmail.com>
2025-01-01 17:13:09 -0300

    Association between Product and Order

--- src/main/java/com/aula01/course/entities/Product.java
+++ src/main/java/com/aula01/course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.aula01.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -30,6 +31,9 @@ public class Product implements Serializable {
             joinColumns = @JoinColumn(name="product_id"),
             inverseJoinColumns = @JoinColumn(name="category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){
 
@@ -87,6 +91,14 @@ public class Product implements Serializable {
         return categories;
     }
 
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
+    }
 
     @Override
     public boolean equals(Object o) {
